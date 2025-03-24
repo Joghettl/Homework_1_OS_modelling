a0758c14480603d7695d70fba65b20fc8003d9e7
Joghettl <87662016+Joghettl@users.noreply.github.com>
2025-03-24 08:47:59 +0100

    Comments in test functions

--- functions_test.py
+++ functions_test.py
@@ -2,6 +2,9 @@ import pytest
 import Profitabilty_PV_system as HW2
 
 def test_calculate_price_n_return():
+    """ 
+    Checks if p and r have the right type.
+    """
     p_supply= 0.3
     p_grid_var= 0.05
     p_grid_fixed= 84.0
@@ -14,12 +17,18 @@ def test_calculate_price_n_return():
     assert isinstance(pnr[1], float),'pnr[1] should be float'
 
 def test_calculate_interestrate():
+    """ 
+    Checks if the interestrate has the right type and is in the expected range.
+    """
     Matrikelnummer = 12133389
     interest_rate = HW2.calculation_interestrate(Matrikelnummer)
     assert isinstance(interest_rate, float),'interest_rate should be float'
-    if(interest_rate>0.1): pytest.raises(ValueError)('interest_rate out of range')
+    if(interest_rate>0.1 or interest_rate<0): pytest.raises(ValueError)('interest_rate out of range')
 
 def test_calculate_npv():
+    """ 
+    Checks if the npv has the right type.
+    """
     p_invest= 640
     life_t= 23
     p= 0.24
